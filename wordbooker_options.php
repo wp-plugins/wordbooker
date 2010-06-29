a28160847cefdc0818853200d9cb4b2e193c80ca
SteveAtty <SteveAtty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-29 21:07:11 +0000

    Minor code tweaks
    git-svn-id: https://plugins.svn.wordpress.org/wordbooker/trunk@258647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordbooker_options.php
+++ wordbooker_options.php
@@ -3,7 +3,7 @@
 /**
 Extension Name: Wordbooker Options 
 Extension URI: http://blogs.canalplan.org.uk/steve
-Version: 1.8.6
+Version: 1.8.9
 Description: Advanced Options for the WordBooker Plugin
 Author: Steve Atty
 */
